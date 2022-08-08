5455ae43485b89a9245ec9198785b56da6d4dd5a
bboyadao <bboyadao@gmail.com>
2022-08-09 00:19:05 +0700

    less log by 2>&1

--- VagrantSetup/k8s_worker.sh
+++ VagrantSetup/k8s_worker.sh
@@ -1,8 +1,8 @@
 cd /vagrant/open_sources/
-tar -xvf sshpass-1.08.tar.gz
+tar -xvf sshpass-1.08.tar.gz >/dev/null 2>&1
 cd sshpass-1.08
-./configure
-make && make install
+./configure >/dev/null 2>&1
+make && make install >/dev/null 2>&1
 
 echo "[TASK 1] JOIN NODE"
 /usr/local/bin/sshpass -p "admin123" scp -o PreferredAuthentications="password" -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no master.example.com:/joincluster.sh /joincluster.sh
